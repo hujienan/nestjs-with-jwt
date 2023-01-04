e06ba2625ad4753676a7d98a9218d741fb550a99
Jet Hu <774372605@qq.com>
2023-01-04 10:02:16 +0800

    set up prisma with mysql; created modules; on JWT

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+    // save the new user in the bd
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+        },
+      });
+      return user;
+    } catch (error) {}
+  }
+}
